d945694da169bf4bfff4dc7fe0f02e698a1a778f
ilae <lmh.hopkins@gmail.com>
2021-04-15 09:08:14 -0500

    Create CelestialObject Model

---
+++ StarChart/Models/CelestialObject.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema
+using System.ComponentModel.DataAnnotations;
+
+public class CelestialObject
+{
+	public CelestialObject()
+	{
+	}
+
+	public int Id;
+
+	[Required]
+	public string Name;
+
+	public int OrbitalObjectId;
+
+	[NotMapped]
+	public List<CelestialObject> Satellites;
+
+	public TimeSpan OrbitalPeriod;
+}
